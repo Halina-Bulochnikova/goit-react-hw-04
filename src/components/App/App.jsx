471dec5704d63e94462f2dda8be5a30bc973a7c9
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-04-09 00:07:07 +0200

    add jsxx files, add css files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,12 @@ import { useState, useEffect } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 
 
-
 const App = () => {
 
  
 return (
   <SearchBar />
+  
   )
 }
 export default App;
--- src/components/servis/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchHits = async () => {
-  const response = await axios.get(
-    `https://api.unsplash.com/photos/?client_id=MH5nMDsKMx575nnwA8p1FeKChrTHHmhgENkd23kCa1s`
-  );
-  return response.data;
-};
